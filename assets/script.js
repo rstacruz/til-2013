f32bd79304f0c4a52e4d4fef82ae101d20bb0917
Rico Sta. Cruz <hi@ricostacruz.com>
2015-05-28 15:12:33 +0800

    Script

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+(function() {
+  var articles = document.querySelectorAll('.next-article');
+  for (var i = 0; i < articles.length; i++) {
+    var article = articles[i];
+    var n = (Math.random()*6)|0+1;
+    article.className += '-v' + n;
+  }
+})();
