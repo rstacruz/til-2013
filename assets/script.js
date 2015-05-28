e6cdf96c7b7468f53f5f2cf56ec0b6c13f6d0092
Rico Sta. Cruz <hi@ricostacruz.com>
2015-05-28 16:18:36 +0800

    Hashy

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,19 @@
   var articles = document.querySelectorAll('.next-article');
   for (var i = 0; i < articles.length; i++) {
     var article = articles[i];
-    var n = (Math.random()*6)|0+1;
+    var id = article.getAttribute('data-id');
+    var n = (hashify(id) % 6) + 1;
     article.className += ' -v' + n;
+  }
+
+  function hashify(str) {
+    var hash = 0, i, chr, len;
+    if (str.length === 0) return hash;
+    for (i = 0, len = str.length; i < len; i++) {
+      chr   = str.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0;
+    }
+    return Math.abs(hash);
   }
 })();
